4e09a9f092e2e7cad8a5980d2ce6f6bf770aed85
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-05-21 14:45:53 +0530

    update

--- app.js
+++ app.js
@@ -92,7 +92,8 @@
 // npm init -y (everything default)
 
 
-const _ = require('lodash')
+const _ = require('lodash');
+const { EventEmitter } = require('stream');
 
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items);
@@ -116,3 +117,33 @@ console.log(newItems);
 // which packages it uses,
 
 // how to run it,
+
+// for more info visit nodesource website
+
+// -------------------------------
+//  CORE CONCEPTS 
+
+// EVENT LOOP 
+// ASYNC PATTERNS
+// EVENTS EMITTER 
+// STREAMS 
+// ------------------------------
+
+// 1.EVENT LOOP 
+
+// the event loop is what allows Node.js to perform non-blocking I/O operations -despite the fact that JS is single threaded - by offloading operations to the system kernel whenever possible 
+
+// 💡 Blocking I/O vs. Non-Blocking I/O
+
+// Feature                                           Blocking I/O	                                      Non-Blocking I/O
+
+// Behavior	                         Waits (blocks) until the task is done	               Does not wait, continues execution
+// Example language	              Java (by default), Python (without asyncio)	           Node.js, Go, JavaScript in browsers
+// Execution style	                                   Synchronous	                                         Asynchronous
+// Scalability	                                Poor for high concurrency	                   Great for handling many requests
+// Use case	                              Simple scripts or CPU-bound tasks	        Web servers, APIs, real-time apps (chat, etc.)
+
+
+//🧠Node.js is single-threaded at the JavaScript level but multi-threaded underneath.
+// ----------------------------
+
