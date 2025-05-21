bf3bcb87af34da88eef5ca5e2aa9383813db84b8
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-05-21 21:45:51 +0530

    add new files

--- event-loop-examples/setTimeout.js
+++ event-loop-examples/setTimeout.js
@@ -6,3 +6,5 @@ setTimeout(() =>{
 },0)
 console.log('third');
 // completed and exited operating systen process
+
+// the order is first -> third -> second as the setTimeout is asynchronous so due to event loop in node.js ,it execute the synchornous code first and then it goes for asynchornous code (offloading principle).
