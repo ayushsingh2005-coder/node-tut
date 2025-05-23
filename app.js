ee95aeaefa6082ab44fe933dd34d7739b5ead950
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-05-23 21:05:03 +0530

    no

--- app.js
+++ app.js
@@ -266,15 +266,13 @@
 
 // code start ------------------------------
 
-// const {createReadStream} =require('fs')
+// const {createReadStream} = require('fs')
 
 // const stream = createReadStream('./content/big.txt');
 
 
-
 // stream.on('data',(result)=>{
-//     console.log(result);
-    
+//     console.log(result);   
 // })
 // stream.on('error' ,(err)=>{console.log(err);
 // } )
