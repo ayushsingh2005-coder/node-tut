0d7f62775b3a1d374b17b592083d736fda14b576
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-05-23 10:24:35 +0530

    main file

--- app.js
+++ app.js
@@ -260,7 +260,7 @@
 
 // 🔸🔸🔸 EVENTS 🔸🔸🔸🔸
 
-// In browser-based JavaScript applications, a major part of your code responds to user actions — like:
+// In browser-based JavaScript applications, a major part of our code responds to user actions — like:
 
 // Clicking a button
 // Hovering over a link
@@ -273,14 +273,76 @@
 
 // listen for specific events and register functions that will execute in response to those events so our events will take place callback function fires.
 
-//🔸setuping up our own events
+//🔸setuping up our own events-------
 
 //🔸EventEmitter is a class in Node.js it allows us to create, listen for, and handle custom events .
-
+// we have two options :
+//  if we wanna create something custom ,we need to extend this class.
+// if we want to emit an event as well as listen for it ,then we can create an instance .
+// -----------------------------------
 const EventEmitter = require('events');
+// ----------------------------------
+// Create an instance (object)(BASIC SETUP)
+const customEmitter = new EventEmitter();
+
+
+customEmitter.on('response',(name,id)=>{
+    console.log(`data recieved ${id} ${name}`);   
+})
+customEmitter.on('res',(name,id)=>{
+    console.log(`some other name here ${name} ${id} `);   
+})
+
+customEmitter.emit('response',"John",1)
+customEmitter.emit('res',"cena",1)
+
+
+// --------------------------------
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//🎯IMPORTANT POINTS -----------------
+
+//🎯objectname.method(event name) 
+
+// syntax :.on and emit method >>>>
+
+//📌M1 : .on(eventName, callback)
+// Registers (listens to) an event.
+
+//📌M2 : .emit(eventName, arg1, arg2, ...)
+// Emits (triggers)  the event, passing optional data to the listener.
+
+//📌WE Can have as many functions as many functions we would want here that will listen for an event and do some other logic for this event
+
+//📌 ORDER MATTERS : first listen ---> then emit (trigger)
+
+//🎯NOTES :
+
+//📌LISTENER : just a CALLBACK FUNCTION that waits for a specific event to happen.
+// When the event happens (emitted), the listener runs automatically.
+
+//📌REGISTER FUNTIONS :
+// A register function is a function used to register a listener — meaning you're telling the program:
+// "Hey, when this event happens, run this function."
+//📌.on() method is the register function.
